2482efe2e8ce383141c9468db611fbc55e3615e7
theroguevigilante <naimish82@proton.me>
2026-07-22 22:51:40 +0530

    Remove add_digit and permuation-string

--- add_digit.rs
+++
@@ -1,9 +0,0 @@
-impl Solution {
-    pub fn add_digits(num: i32) -> i32 {
-        match (num, num % 9) {
-            (0, _) => 0,
-            (_, 0) => 9,
-            (_, r) => r
-        }
-    }
-}
